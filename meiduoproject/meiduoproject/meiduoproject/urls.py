4abf5869598bf9e18aab5f31b008dd267422d401
doushuhao <doushuhao@itcast.cn>
2020-08-07 16:51:06 +0800

    工程建立

--- meiduoproject/meiduoproject/meiduoproject/urls.py
+++ meiduoproject/meiduoproject/meiduoproject/urls.py
@@ -1,4 +1,4 @@
-"""meiduoproject1 URL Configuration
+"""meiduoproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
